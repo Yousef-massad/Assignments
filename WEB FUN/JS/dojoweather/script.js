9e8fa332076dafa4f8921ea9c0ea24fa756b7c69
yousef <yousef.d.massad@gmail.com>
2026-09-16 17:31:08 +0300

    dojoweather finished

--- WEB FUN/JS/dojoweather/script.js
+++ WEB FUN/JS/dojoweather/script.js
@@ -6,3 +6,44 @@ function hide() {
     var foot = document.querySelector("footer")
     foot.remove();
 }
+
+var current="c"
+
+function changeTemp(){
+    var sel= document.getElementById("room2")
+    var red=document.querySelectorAll(".red")
+    var blue=document.querySelectorAll(".blue")
+
+    console.log("hello")
+    if(sel.value !== current){
+
+        if(sel.value === "f"){
+       
+            for(var i=0; i<red.length; i++){
+                var rep=Number(red[i].textContent.replace("°",""))
+                var fah=rep*9/5+32
+                red[i].innerText=fah + "°"
+
+                var rep=Number(blue[i].textContent.replace("°",""))
+                var fah=rep*9/5+32
+                blue[i].innerText=fah + "°"
+            }
+
+            current="f"
+        }
+
+        else{
+            for(var i=0; i<red.length; i++){
+                var rep=Number(red[i].textContent.replace("°",""))
+                var cel=Math.round((rep - 32) * 5 / 9 * 10) / 10
+                red[i].innerText=cel + "°"
+
+                var rep=Number(blue[i].textContent.replace("°",""))
+                var cel=Math.round((rep - 32) * 5 / 9 * 10) / 10
+                blue[i].innerText=cel + "°"
+            }
+
+            current="c"
+        }
+    }
+}
